dedc0da9cc528c38e931a5332a53cfe5b9da37d4
mateus-neres <mateusneres03@gmail.com>
2023-08-23 22:12:03 -0300

    Create Lista02Atividade02.py

---
+++ Comandos_Condicionais/Lista02Atividade02.py
@@ -0,0 +1,7 @@
+'''
+2. Escreva um programa que receba como entrada um número e exiba uma mensagem
+informando se ele é par ou ímpar.
+Lembrete: Um número é par se for divisível por dois, ou seja, se na divisão inteira por dois
+o resto for zero. Caso contrário, ele será ímpar.
+
+'''
