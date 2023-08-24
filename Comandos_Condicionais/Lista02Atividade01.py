1645ff133324e38d89c1b95eaf3b8afbaff03836
mateus-neres <mateusneres03@gmail.com>
2023-08-23 22:11:32 -0300

    Create Lista02Atividade01.py

---
+++ Comandos_Condicionais/Lista02Atividade01.py
@@ -0,0 +1,9 @@
+'''
+1. Escreva um programa que receba como entrada um número e exiba uma mensagem
+informando se ele é positivo, negativo ou neutro.
+Lembrete: Os números maiores que zero são chamados de positivos, enquanto os
+números menores que zero são os negativos. Zero é um número neutro.
+
+
+
+'''
