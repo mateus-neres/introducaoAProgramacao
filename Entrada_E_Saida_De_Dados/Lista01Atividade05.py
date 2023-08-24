ab7637efca3a962488e3d994b04d4e0900f3ec0a
mateus-neres <mateusneres03@gmail.com>
2023-08-24 07:35:44 -0300

    Delete Lista01Atividade05.py

--- Entrada_E_Saida_De_Dados/Lista01Atividade05.py
+++
@@ -1,6 +0,0 @@
-'''
-5. Um restaurante self-service de Rio Tinto cobra R$ 40 por quilo nas refeições. Sabendo que, na hora
-de determinar o valor da refeição, deve ser desconsiderado o peso do prato vazio (230 gramas),
-escreva um programa que receba como entrada o peso total do prato de um cliente em gramas e
-exiba o preço cobrado com duas casas decimais. (Lembrete: 1 quilo = 1000 gramas)
-'''
