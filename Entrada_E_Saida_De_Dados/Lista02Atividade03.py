e213a7da757c56e2dd8e7c689615b4e01d39c408
mateus-neres <mateusneres03@gmail.com>
2023-08-23 22:14:43 -0300

    Create Lista02Atividade03.py

---
+++ Entrada_E_Saida_De_Dados/Lista02Atividade03.py
@@ -0,0 +1,6 @@
+'''
+3) Regiane vai comemorar o aniversário de seu filho e precisa encomendar docinhos. Cada brigadeiro
+custa R$ 1,90 e cada beijinho custa R$ 1,70. Escreva um programa que receba como entrada a
+quantidade de brigadeiros e de beijinhos pedidos, e também a quantidade de crianças convidadas,
+e exiba o valor total gasto, e a quantidade total de docinhos que cada criança vai comer.
+'''
