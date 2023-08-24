751e0a1ff317c60bf0d4b9029d7fd13a01cd8fb7
mateus-neres <mateusneres03@gmail.com>
2023-08-23 22:15:11 -0300

    Create Lista03Atividade04.py

---
+++ Entrada_E_Saida_De_Dados/Lista03Atividade04.py
@@ -0,0 +1,6 @@
+'''
+4) Sheldon tem uma grande coleção de revistas em quadrinhos raras e resolveu doá-las a seus amigos
+(Bazinga!!!). Escreva um programa que receba como entrada a quantidade total de revistas e a
+quantidade de amigos que serão beneficiados, e exiba quantas revistas cada um vai receber e
+quantas sobrarão para Sheldon.
+'''
