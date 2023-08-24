344534394f3c5165092c97df1152cb02ee1ece11
mateus-neres <mateusneres03@gmail.com>
2023-08-24 07:35:07 -0300

    Create Lista01Atividade01-TipoDeDado.py

---
+++ Entrada_E_Saida_De_Dados/Lista01Atividade01-TipoDeDado.py
@@ -0,0 +1,16 @@
+'''
+1. Para cada dado abaixo, indique qual tipo (int, float ou String) seria mais apropriado para seu
+armazenamento:
+
+Dado Tipo
+Quantidade de livros da biblioteca -> int
+Título de um filme -> str
+Ano de nascimento de um aluno -> int
+Peso de um animal (em quilos) -> float
+Signo de uma pessoa -> str
+Cor de um tecido -> str
+Salário de um professor -> str
+Total de habitantes de uma cidade -> int
+Altura de um prédio (em metros) -> int
+Nome de um brinquedo -> str
+'''
