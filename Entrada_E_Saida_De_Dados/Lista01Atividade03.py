a12390acfa3826e7335214fa2eeb0bb9557d1752
mateus-neres <mateusneres03@gmail.com>
2023-08-24 07:35:29 -0300

    Delete Lista01Atividade03.py

--- Entrada_E_Saida_De_Dados/Lista01Atividade03.py
+++
@@ -1,6 +0,0 @@
-'''
-3. Sabendo que a Copiadora de Rio Tinto cobra R$ 0,15 por cada cópia feita, escreva um programa
-que receba como entrada a quantidade de folhas de um livro e exiba o valor total a ser pago para
-copiá-lo com duas casas decimais. (Lembrete: cada folha corresponde a duas páginas - frente e
-verso)
-'''
