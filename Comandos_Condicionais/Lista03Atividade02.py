0469fd750f0749852d736b7a9bf55af5c5b5eb26
mateus-neres <mateusneres03@gmail.com>
2023-08-23 22:13:33 -0300

    Create Lista03Atividade02.py

---
+++ Comandos_Condicionais/Lista03Atividade02.py
@@ -0,0 +1,15 @@
+'''
+2. Vladimir trabalha em uma papelaria da cidade que oferece serviços de xérox e encadernação. Os
+preços cobrados são os seguintes:
+
+Serviço Preço
+Xérox em preto e branco R$ 0,06 (cada)
+Xérox colorida R$ 0,29 (cada)
+Encadernação (até 100 folhas) R$ 2,00
+Encadernação (mais de 100 folhas) R$ 4,00
+
+Escreva um programa que receba como entrada o tipo de serviço (xérox ou encadernação) preferido
+por um cliente, e a quantidade de páginas para copiar ou de folhas para encadernar. O programa
+deverá também calcular e exibir o valor total a ser pago. Observe que, caso o usuário opte por xérox,
+será necessário receber como entrada também o tipo de xérox (PB ou colorida).
+'''
