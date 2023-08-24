706f6290082081ea30b8930cb5cee44e49533a1a
mateus-neres <mateusneres03@gmail.com>
2023-08-23 22:13:42 -0300

    Create Lista03Atividade04.py

---
+++ Comandos_Condicionais/Lista03Atividade04.py
@@ -0,0 +1,8 @@
+'''
+4. Uma locadora de veículos cobra seus clientes por eventuais atrasos na devolução dos carros alugados.
+Caso o atraso seja inferior a duas horas, será cobrada uma multa de R$ 30. Para atrasos de até três
+horas, a multa é de R$ 80. Se o atraso for superior a três horas, a multa será de R$ 40 por hora. Escreva
+um programa que receba como entrada o valor da diária do veículo alugado, a quantidade de dias do
+aluguel, e o tempo de atraso na devolução em horas, e exiba o valor total a ser pago. (Dica: nem todos
+os clientes devolvem os veículos com atraso)
+'''
