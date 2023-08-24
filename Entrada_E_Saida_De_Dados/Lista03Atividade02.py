19f69596b19733bf9c2b8e1d4190d6f5b48f42eb
mateus-neres <mateusneres03@gmail.com>
2023-08-23 22:15:01 -0300

    Create Lista03Atividade02.py

---
+++ Entrada_E_Saida_De_Dados/Lista03Atividade02.py
@@ -0,0 +1,7 @@
+'''
+2) Betina trabalha em um escritório de advocacia e todos os dias analisa vários processos. Sabendo
+que ela cumpre um expediente diário de 8h, escreva um programa que receba como entrada
+quantos minutos ela leva para analisar cada processo, e exiba o total de processos revisados em um
+dia de trabalho. (Dica: Uma hora tem 60 minutos, e considere que um processo não pode ser
+analisado parcialmente, apenas totalmente)
+'''
