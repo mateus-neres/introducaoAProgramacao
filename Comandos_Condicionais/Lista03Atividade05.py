219f8173f7e606005b77ec9a53200330921265ae
mateus-neres <mateusneres03@gmail.com>
2023-08-23 22:13:45 -0300

    Create Lista03Atividade05.py

---
+++ Comandos_Condicionais/Lista03Atividade05.py
@@ -0,0 +1,8 @@
+'''
+5. Helena trabalha na bilheteria de um cinema, e vende ingressos e lanches. Os ingressos para filmes em
+2D custam R$ 8,50, e para os filmes em 3D custam R$ 14,50. Caso queira adquirir um combo simples
+(pipoca e refrigerante), o cliente deve pagar mais R$ 10, e se preferir um combo completo (pipoca,
+refrigerante e chocolate) deve desembolsar mais R$ 12.
+Escreva um programa que receba como entrada as escolhas de cada cliente e exiba o valor total a ser
+pago. (Dica: nem todo cliente que vai ver filme compra lanche)
+'''
