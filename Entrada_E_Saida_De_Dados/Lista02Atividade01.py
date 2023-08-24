3e356a1e294af3c55c237e5681aceb8641743b77
mateus-neres <mateusneres03@gmail.com>
2023-08-24 14:35:25 -0300

    Delete Lista02Atividade01.py

--- Entrada_E_Saida_De_Dados/Lista02Atividade01.py
+++
@@ -1,5 +0,0 @@
-'''
-1) Uma dúzia de laranjas custa R$ 9,75 no supermercado mais próximo. Escreva um programa que
-receba como entrada a quantidade desejada de laranjas e exiba o valor total necessário para
-comprá-las. (Dica: uma dúzia corresponde a 12 laranjas. Descubra o preço unitário)
-'''
