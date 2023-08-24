661a5ffdde956887b65125ed1f93f6219e3a910e
mateus-neres <mateusneres03@gmail.com>
2023-08-24 14:35:34 -0300

    Delete Lista02Atividade02.py

--- Entrada_E_Saida_De_Dados/Lista02Atividade02.py
+++
@@ -1,7 +0,0 @@
-'''
-2) Otávio resolveu organizar um churrasco para comemorar seu aniversário. A estimativa é que cada
-pessoa consuma 400 gramas de carne e 6 latas de cerveja. Pelas pesquisas que ele fez no
-supermercado de seu bairro, o quilo de carne custará R$ 41 e cada cerveja sairá por R$ 5,20.
-Escreva um programa que receba como entrada a quantidade de pessoas que irão para o churrasco
-e exiba o valor total que ele gastará com carne e cerveja com duas casas decimais cada um.
-'''
