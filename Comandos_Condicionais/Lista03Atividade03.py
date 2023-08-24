124572a1d8cfcd0f76d7a19c2162ca0e938471b3
mateus-neres <mateusneres03@gmail.com>
2023-08-23 22:13:38 -0300

    Create Lista03Atividade03.py

---
+++ Comandos_Condicionais/Lista03Atividade03.py
@@ -0,0 +1,14 @@
+'''
+3. Pensando no grande número de motoristas que viajam no feriado, o posto de combustíveis Abasteça Já
+resolveu fazer uma superpromoção, oferecendo preços bem abaixo do mercado:
+
+Tipo de Combustível Valor por Litro
+Gasolina R$ 2,53
+Etanol R$ 2,09
+Diesel R$ 1,92
+
+Além disso, todos os clientes que abastecerem seus veículos com mais de 30 litros de etanol serão
+premiados com uma troca de óleo. Escreva um programa para esse posto de modo que ele receba
+como entrada o combustível escolhido e o valor em dinheiro que se deseja gastar, e informe o total de
+combustível abastecido e uma mensagem indicando se o cliente ganhou ou não a troca de óleo.
+'''
