89b44f551848cd8bc9456a074f615534520d9abf
mateus-neres <mateusneres03@gmail.com>
2023-08-23 22:14:17 -0300

    Create Lista04Atividade05.py

---
+++ Comandos_Condicionais/Lista04Atividade05.py
@@ -0,0 +1,9 @@
+'''
+5. O INSS precisa de um sistema para calcular o total gasto com o pagamento de benefícios. O
+tempo mínimo de trabalho para a aposentadoria de uma pessoa é de 55 anos, e o benefício
+pago corresponde a 60% do salário atual dela. Para cada ano trabalhado acima desse tempo, é
+acrescentado um bônus correspondente a 15% do salário atual.
+Escreva um programa que receba como entrada o tempo de serviço e o salário atual de um
+empregado e exiba o benefício concedido a ele. Funcionários com tempo de serviço abaixo do
+mínimo exigido devem receber uma mensagem informando que não têm direito ao benefício.
+'''
