e9e8607ec871bf9fa17d80521a0b17e5480088d2
mateus-neres <mateusneres03@gmail.com>
2023-08-23 22:14:20 -0300

    Create Lista01Atividade01.py

---
+++ Entrada_E_Saida_De_Dados/Lista01Atividade01.py
@@ -0,0 +1,16 @@
+'''
+1. Para cada dado abaixo, indique qual tipo (int, float ou String) seria mais apropriado para seu
+armazenamento:
+
+Dado Tipo
+Quantidade de livros da biblioteca
+Título de um filme
+Ano de nascimento de um aluno
+Peso de um animal (em quilos)
+Signo de uma pessoa
+Cor de um tecido
+Salário de um professor
+Total de habitantes de uma cidade
+Altura de um prédio (em metros)
+Nome de um brinquedo
+'''
