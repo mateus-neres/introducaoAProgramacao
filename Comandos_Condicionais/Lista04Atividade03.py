92a5e09cb51d718ea2027e8f92a031fdad7dd7ba
mateus-neres <mateusneres03@gmail.com>
2023-08-23 22:14:10 -0300

    Create Lista04Atividade03.py

---
+++ Comandos_Condicionais/Lista04Atividade03.py
@@ -0,0 +1,10 @@
+'''
+3. Luzia é fã do Cirque du Soleil e desde que soube que haverá apresentações no Brasil, tem
+mobilizado amigos e familiares para ir. Ela já pesquisou informações sobre transporte e
+ingressos, e agora precisa organizar o orçamento da viagem.
+Há três tipos de ingresso, dependendo do setor escolhido: platéia VIP (R$ 350), cadeira (R$
+200) e arquibancada (R$ 100). Estudantes têm direito a 50% de desconto (meia-entrada) em
+todos os setores, exceto na platéia VIP. Também é cobrada uma taxa de conveniência
+correspondente a 5% do valor do ingresso. Escreva um programa que receba como entrada o
+setor escolhido e o tipo de ingresso (inteira ou meia) e exiba o valor total a ser pago.
+'''
