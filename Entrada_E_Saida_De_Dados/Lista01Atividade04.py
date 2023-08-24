1f77b254fb7fae1f7535c774b8d1561e5a6cf43b
mateus-neres <mateusneres03@gmail.com>
2023-08-23 22:14:30 -0300

    Create Lista01Atividade04.py

---
+++ Entrada_E_Saida_De_Dados/Lista01Atividade04.py
@@ -0,0 +1,7 @@
+'''
+4. A Escola Legal paga seus professores considerando as aulas que eles dão e os projetos que
+orientam. Sabendo que são pagos R$ 35 por cada hora de aula, e R$ 80 por cada projeto, escreva
+um programa que receba como entrada a quantidade de horas em que um professor esteve em
+sala de aula no mês, e a quantidade de projetos orientados por ele, e exiba o salário desse
+professor.
+'''
