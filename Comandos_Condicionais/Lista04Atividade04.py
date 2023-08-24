3df33a64bc20e2a5b90841927ffdb8e938a02cd1
mateus-neres <mateusneres03@gmail.com>
2023-08-23 22:14:14 -0300

    Create Lista04Atividade04.py

---
+++ Comandos_Condicionais/Lista04Atividade04.py
@@ -0,0 +1,12 @@
+'''
+4. Soraia está organizando uma grande festa de reencontro de sua turma de colégio. Ela precisa
+arrecadar o pagamento dos participantes (ex-alunos e convidados) para poder viabilizar a
+festa. Cada ex-aluno deverá pagar R$ 60 e poderá levar convidados, que pagarão taxas de
+acordo com suas idades. Convidados com 12 anos ou mais pagarão R$ 50, enquanto
+convidados com idade entre 3 e 11 anos pagarão R$ 25, e convidados com menos de 3 anos
+não pagarão nada.
+Escreva um programa que receba como entrada o tipo de participante (ex-aluno ou
+convidado), e a idade quando necessário, e exiba o valor a ser pago.
+Obs: Lembre-se que o usuário pode usar maiúsculas e minúsculas nos dados de entrada.
+Obs2: No caso dos ex-alunos, a idade não deve ser pedida na entrada de dados.
+'''
