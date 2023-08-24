e8803ce2fb875d217c806968aafc061f90fe6859
mateus-neres <mateusneres03@gmail.com>
2023-08-23 22:14:24 -0300

    Create Lista01Atividade02.py

---
+++ Entrada_E_Saida_De_Dados/Lista01Atividade02.py
@@ -0,0 +1,8 @@
+'''
+2. A biblioteca de Rio Tinto empresta gratuitamente seus livros a alunos, professores e funcionários
+de todo o campus. Entretanto, sempre que um usuário atrasa a entrega de um livro, ele tem que
+pagar uma multa de R$ 2,50 por cada dia de atraso.
+Escreva um programa que receba como entrada a quantidade de dias de atraso do empréstimo de
+um livro, e exiba o valor da multa a ser paga pelo usuário com duas casas decimais.
+
+'''
