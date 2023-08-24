a5db7c4c98b9477e0404fcf32cc7036b56166707
mateus-neres <mateusneres03@gmail.com>
2023-08-23 22:14:05 -0300

    Create Lista04Atividade02.py

---
+++ Comandos_Condicionais/Lista04Atividade02.py
@@ -0,0 +1,9 @@
+'''
+2. Jandira vende celulares e tablets em uma loja no shopping. No momento, ela tem dois
+modelos de celular em estoque: Motorola (custando R$ 879,18) e Samsung (custando R$
+921,40). Tem também dois tipos de tablets: Multilaser (preço: R$ 339,50) e Samsung (preço: R$
+417,72).
+Escreva um programa que receba como entrada o tipo de aparelho, o modelo escolhido e a
+quantidade desejada por um cliente, e exiba o valor total a ser pago.
+Obs: Lembre-se que o usuário pode usar maiúsculas e minúsculas nos dados de entrada.
+'''
