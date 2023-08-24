9f4f071af0038d86bd4c3751832db45ad000af03
mateus-neres <mateusneres03@gmail.com>
2023-08-24 18:01:16 -0300

    Atualização dos desafios

---
+++ Estruturas_De_Repeticoes/Lista02Atividade02.py
@@ -0,0 +1,7 @@
+'''
+2. Escreva um programa que receba como entrada vários números, até que seja informado o valor 100, e exiba a
+média dos números pares.
+Dica: a média dos números pares é calculada dividindo-se a soma dos pares pela quantidade de números pares.
+Porém, antes de dividir, é preciso testar se a quantidade é maior que zero, pois pode ser que não tenham sido
+lidos números pares.
+'''
---
+++ Estruturas_De_Repeticoes/Lista02Atividade04.py
@@ -0,0 +1,6 @@
+'''
+4. Devido aos altos índices de poluição, uma cidade resolveu multar as casas que possuem mais de 2 veículos em R$
+10 por mês por cada veículo extra. Escreva um programa que receba como entrada a quantidade de veículos de
+várias residências, até que seja informado o valor 555, e exiba a multa de cada casa e o total mensal arrecadado
+com as multas.
+'''
