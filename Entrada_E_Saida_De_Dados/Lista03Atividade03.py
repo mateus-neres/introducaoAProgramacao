ce82a8418ca2bde446f0768baee1e42db338c364
mateus-neres <mateusneres03@gmail.com>
2023-08-23 22:15:08 -0300

    Create Lista03Atividade03.py

---
+++ Entrada_E_Saida_De_Dados/Lista03Atividade03.py
@@ -0,0 +1,6 @@
+'''
+3) Janete vai comemorar seu aniversário numa pizzaria muito badalada. Conversando com o dono, ela
+conseguiu uma promoção: a cada 10 rodízios, um será gratuito. Escreva um programa que receba
+como entrada a quantidade de pessoas convidadas e o preço do rodízio, e exiba o total a ser pago
+com duas casas decimais. (Dica: pense em divisão!)
+'''
