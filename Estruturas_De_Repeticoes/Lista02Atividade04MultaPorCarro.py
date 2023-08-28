39f2c16f82a477cbf0aef322843e5f37f752e1c3
mateus-neres <mateusneres03@gmail.com>
2023-08-28 07:15:51 -0300

    Solução do desafio lista02Atividade04

--- Estruturas_De_Repeticoes/Lista02Atividade04MultaPorCarro.py
+++ Estruturas_De_Repeticoes/Lista02Atividade04MultaPorCarro.py
@@ -4,20 +4,27 @@
 várias residências, até que seja informado o valor 555, e exiba a multa de cada casa e o total mensal arrecadado
 com as multas.
 '''
+# Variaveis
 casa = 0
 multa = 10
 total_arrecadado = 0
 multa_mensal = 0
-Lite_qtd_veiculo = 2
+Limite_qtd_veiculo = 2
 
+# Entra de dados
 qtd_veiculo = int(input('Quantos veículos tem em sua residência: '))
 
+# Comando de repetição
 while qtd_veiculo != 555:
-    total_arrecadado = total_arrecadado + multa_mensal
-    if qtd_veiculo > 2:
-       multa_mensal = (qtd_veiculo - Lite_qtd_veiculo) * multa
-       
-       print(f'O valor da multa da casa {casa} é de R${multa_mensal:.2f}.')
+    
+    if qtd_veiculo > 2: 
+
+       multa_mensal = (qtd_veiculo - Limite_qtd_veiculo) * multa 
+       total_arrecadado += multa_mensal
        casa += 1
+       print(f'O valor da multa da casa {casa} é de R${multa_mensal:.2f}.')
+       qtd_veiculo = int(input('Quantos veículos tem em sua residência: '))
+       
     qtd_veiculo = int(input('Quantos veículos tem em sua residência: '))
-print(f'O valor arrecadado total é de: R${multa_acumulada:.2f}.')
+    casa += 1   
+print(f'O valor arrecadado total é de: R${total_arrecadado:.2f}.')
