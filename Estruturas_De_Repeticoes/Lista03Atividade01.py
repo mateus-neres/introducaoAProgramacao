0254ae40b25cf8debba77917e534b194f95c9604
mateus-neres <mateusneres03@gmail.com>
2023-08-29 16:54:40 -0300

    Novos desafios
    Novos desafios de estruturas de repetições

---
+++ Estruturas_De_Repeticoes/Lista03Atividade01.py
@@ -0,0 +1,5 @@
+'''
+1. Escreva um programa que receba como entrada vários números, até que o usuário não deseje mais
+informar, e exiba a soma dos que são múltiplos de 3. (Obs: as entradas da tabela indicam o número e a
+resposta do usuário a cada repetição)
+'''
---
+++ Estruturas_De_Repeticoes/Lista03Atividade02.py
@@ -0,0 +1,7 @@
+'''
+2. Hoje à noite, o cinema da cidade será inaugurado com uma promoção: crianças até 10 anos e idosos
+com mais de 60 anos não pagarão ingresso. Escreva um programa que receba como entrada a idade de
+cada pessoa que foi ao cinema até que o usuário não deseje mais informar dados, e exiba a quantidade
+de pessoas que tiveram entrada gratuita. (Obs: as entradas da tabela indicam a idade e a resposta do
+usuário a cada repetição)
+'''
