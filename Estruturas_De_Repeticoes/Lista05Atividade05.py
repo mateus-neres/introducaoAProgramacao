079ef7a5b9f1d2f22a73529bad89b42b7374ccc1
mateus-neres <mateusneres03@gmail.com>
2023-09-06 13:24:40 -0300

    Resolução Lista05Atividade05

--- Estruturas_De_Repeticoes/Lista05Atividade05.py
+++ Estruturas_De_Repeticoes/Lista05Atividade05.py
@@ -3,3 +3,12 @@
 empresa, até que seja informada uma quantidade negativa, e exiba a quantidade média de filhos do
 grupo. (Dica: a média pode ser obtida dividindo-se a soma dos números pela quantidade deles)
 '''
+contador = 0
+soma_filhos = 0
+qtd_filhos = 0
+while qtd_filhos >= 0:
+    qtd_filhos = int(input("Quantos filhos você tem ? "))
+    soma_filhos += qtd_filhos
+    contador += 1
+media_filhos = soma_filhos / contador
+print(f"Media total de filhos {contador}")
