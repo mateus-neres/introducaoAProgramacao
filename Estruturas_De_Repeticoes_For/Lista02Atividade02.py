ac43e7ad2adca4bc9afb0666a2357d871890f02c
mateus-neres <mateusneres03@gmail.com>
2023-09-18 21:56:41 -0300

    Resolução de desafio:
    Estruturas de repetições "for".
    
    Lista02Atividade2
    
    Eleições.

--- Estruturas_De_Repeticoes_For/Lista02Atividade02.py
+++ Estruturas_De_Repeticoes_For/Lista02Atividade02.py
@@ -7,3 +7,37 @@ Escreva um programa que receba como entrada os votos de 100 alunos e exiba uma m
 informando o nome do eleito. Caso não haja nenhum vencedor, deverá ser exibida uma
 mensagem informando que será necessária uma nova votação.
 '''
+
+# Variaveis para contagem dos pontos de votação
+branco = 0
+marcilio = 0
+aurelio = 0
+
+# Estrutura de repetição FOR
+for contador in range(1,100):
+    # Entrada de dados do usuário
+    voto = str.upper(input("Digite seu voto: \nMarcílio\nAurélio\nBranco\nVoto: "))
+    # Tratamentos de dados do usuario para evitar erros de digitação
+    while voto != "MARCÍLIO" and voto != "AURÉLIO" and voto != "BRANCO":
+        print("Você digitou algo errado, por gentileza, digite apenas")
+        voto = str.upper(input("Marcílio\nAurélio\nBranco\nDigite: "))
+    # Comandos condicionais para contagem de votos
+    if voto == "MARCÍLIO":
+        marcilio += 1
+    elif voto == "AURÉLIO":
+        aurelio += 1
+    elif voto == "BRANCO":
+        branco += 1
+# Saida de dados de acordo com o desafio 
+if aurelio <= branco and marcilio <= branco:
+    print("Nova votação")
+   
+elif aurelio  == marcilio:
+    print("Nova votação")
+    
+elif aurelio > marcilio and aurelio > branco:
+    print("Aurélio")
+   
+elif marcilio > aurelio and marcilio > branco:
+    print("Marcílio")
+    
