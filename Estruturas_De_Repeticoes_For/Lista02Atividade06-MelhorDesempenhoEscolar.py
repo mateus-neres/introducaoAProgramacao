677d898a8d3d3f411fac829b9f115ac888caceb2
mateus-neres <mateusneres03@gmail.com>
2023-09-19 22:55:23 -0300

    Resolução de desavios "for"
    Lista02Atividade06-MelhorDesempenhoEscolar

--- Estruturas_De_Repeticoes_For/Lista02Atividade06-MelhorDesempenhoEscolar.py
+++ Estruturas_De_Repeticoes_For/Lista02Atividade06-MelhorDesempenhoEscolar.py
@@ -9,5 +9,16 @@ mostrar que ele está se esforçando, cada nota terá que ser superior à anteri
 Escreva um programa que receba como entrada as 6 notas do filho de Talita e exiba uma
 mensagem informando se ele ganhará brinquedo ou não.
 '''
-
-
+menor_nota = 0
+maior_nota = 0
+for contMes in range(1,7):
+    nota = float(input(f"Digite a nota do {contMes}° mês: "))
+    if nota > maior_nota:
+        maior_nota = nota
+        maior_nota += 1
+    elif maior_nota <= nota:
+        menor_nota += 1
+if menor_nota >= 1:
+    print("Não ganha brinquedo")
+else:
+    print("Ganha brinquedo")
--- Estruturas_De_Repeticoes_For/Lista02Atividade07.py
+++ Estruturas_De_Repeticoes_For/Lista02Atividade07.py
@@ -7,3 +7,15 @@ Escreva um programa que receba como entrada a quantidade de correspondências en
 por ele em cada um dos sete dias da semana, e exiba em quantos dias ele cumpriu a meta, e a
 média de entregas diárias que ele fez no período.
 '''
+entregas_acumuladas = 0
+cont_meta = 0
+cont_dias = 0
+for dias in range(1,8):
+    entregas = int(input(f"Quantas correspondências foiram entregues no {dias}°: "))
+    entregas_acumuladas += entregas
+    cont_dias += 1
+    if entregas >= 100:
+        cont_meta += 1
+media = entregas_acumuladas // cont_dias
+print(f"Cumpriu a meta em {cont_meta} dias")
+print(f"Média de entregas: {media}")
