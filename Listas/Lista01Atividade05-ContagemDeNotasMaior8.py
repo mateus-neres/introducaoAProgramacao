d3a77bfecd9da0b24eff5c0a71587ca00fa04af1
mateus-neres <mateusneres03@gmail.com>
2023-09-26 10:09:39 -0300

    Resolução de desafio
    Lista01Atividade05-ContagemDeNotasMaior8

---
+++ Listas/Lista01Atividade05-ContagemDeNotasMaior8.py
@@ -0,0 +1,13 @@
+'''
+5. Escreva um programa que crie uma lista com 5 notas informadas pelo usuário, e exiba quantas
+notas foram acima de 8.
+'''
+
+listaNota = []
+contNota = 0
+for i in range(5):
+    nota = float(input("Digite a nota: "))
+    listaNota += [nota]
+    if nota > 8:
+        contNota += 1
+print(f"Quantidade de notas maiores que 8: {contNota}")
