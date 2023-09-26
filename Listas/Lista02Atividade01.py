f0a8b7b4ba9e652d107182386be537f3776b29bb
mateus-neres <mateusneres03@gmail.com>
2023-09-26 13:03:30 -0300

    Update Lista02Atividade01.py

--- Listas/Lista02Atividade01.py
+++ Listas/Lista02Atividade01.py
@@ -2,3 +2,16 @@
 1. Escreva um programa que receba como entrada quinze letras, armazene-as em uma lista,
 e depois exiba quantas vezes uma letra qualquer informada pelo usuário aparece na lista.
 '''
+
+
+lista = []
+for i in range(15):
+    listaLetra = ["a","b","g","g","j","y","y","y","y","k","l","t","e","e","e",]
+    letra = listaLetra[i]
+    lista += [letra]
+    contador = set(lista)
+    
+
+print(letra)
+print(contador)
+print(lista)
