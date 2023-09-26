01ccf3918419351d59b57fed4b2a921de967e686
mateus-neres <mateusneres03@gmail.com>
2023-09-26 08:52:05 -0300

    Resolução de desafio
    Lista01Atividade01-ResultadosDosPrints

---
+++ Listas/Lista01Atividade01-ResultadosDosPrints.py
@@ -0,0 +1,14 @@
+'''
+1. Considerando o conteúdo das listas a seguir, indique o que será exibido por cada comando:
+
+A = [8, 5, 1, 3, 0]
+B = [7.0, 1.2, 3.9, 4.1]
+C = [10, 2, 6]
+
+a) print (A[0] – B[2] + 1)      => 5.1
+b) print (C[1] + B[3] + 2*A[2]) => 8.1
+c) print (B[3], A[4])           => 4.1 0
+d) print (A[0:3])               => [8, 5, 1]
+e) for i in range (2):
+    print(A[i] + C[i])          => Quando i==0 print será 18 | Quando i==1 print será 7 
+'''
--- Listas/Lista01Atividade01.py
+++
@@ -1,9 +0,0 @@
-'''
-1. Considerando o conteúdo das listas a seguir, indique o que será exibido por cada comando:
-a) print (A[0] – B[2] + 1)
-b) print (C[1] + B[3] + 2*A[2])
-c) print (B[3], A[4])
-d) print (A[0:3])
-e) for i in range (2):
-print(A[i] + C[i])
-'''
