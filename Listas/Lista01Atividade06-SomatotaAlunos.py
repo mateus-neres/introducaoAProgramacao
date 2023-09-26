c96c0fa5510393826e2c1eb0dcf964bc4c83eabc
mateus-neres <mateusneres03@gmail.com>
2023-09-26 10:14:54 -0300

    Resolução de desafio
    Lista01Atividade06-SomatotaAlunos

---
+++ Listas/Lista01Atividade06-SomatotaAlunos.py
@@ -0,0 +1,13 @@
+'''
+6. Escreva um programa que crie uma lista com as quantidades de alunos de 4 turmas, e exiba a
+quantidade total de alunos.
+'''
+
+soma_Aluno = 0
+lista_Aluno_Turma = []
+for i in range(4):
+    qtd_Alunos = int(input("Digite a quantidade de alunos na turma: "))
+    lista_Aluno_Turma += [qtd_Alunos]
+    soma_Aluno += qtd_Alunos
+print(lista_Aluno_Turma)
+print(f"Quantidade total de alunos é de: {soma_Aluno}")
--- Listas/Lista01Atividade06.py
+++
@@ -1,4 +0,0 @@
-'''
-6. Escreva um programa que crie uma lista com as quantidades de alunos de 4 turmas, e exiba a
-quantidade total de alunos.
-'''
