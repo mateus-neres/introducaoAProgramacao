cd980f7fa0ca5f4ceb16034860967e4a4f11f988
mateus-neres <mateusneres03@gmail.com>
2023-09-26 10:04:36 -0300

    Resolução de desafio
    Lista01Atividade04-Fibonacci

---
+++ Listas/Lista01Atividade04-Fibonacci.py
@@ -0,0 +1,16 @@
+'''
+4. Escreva um programa que crie uma lista com 5 números inteiros informados pelo usuário,
+depois some cada elemento com seu próximo, e exiba a lista original e a nova lista resultante.
+(Dica: Atenção para o índice limite da lista)
+'''
+
+lista = []
+listaSoma = []
+for i in range(5):
+    numero = int(input("Digite um numero inteiro: "))
+    lista += [numero]
+    if i >= 1:
+        listaSoma += [lista[i] + lista[i-1]]
+listaSoma.append(lista[-1])
+print(lista)
+print(listaSoma)
--- Listas/Lista01Atividade04.py
+++
@@ -1,5 +0,0 @@
-'''
-4. Escreva um programa que crie uma lista com 5 números inteiros informados pelo usuário,
-depois some cada elemento com seu próximo, e exiba a lista original e a nova lista resultante.
-(Dica: Atenção para o índice limite da lista)
-'''
