cb6b906b6d14213ce03baf0eeea69c869abd3ec2
mateus-neres <mateusneres03@gmail.com>
2023-09-26 09:15:28 -0300

    Resolução de desafio
    Lista01Atividade03-ExibirMultiplosDeTres

---
+++ Listas/Lista01Atividade03-ExibirMultiplosDeTres.py
@@ -0,0 +1,14 @@
+'''
+3. Escreva um programa que crie uma lista com 8 números inteiros informados pelo usuário e
+exiba apenas aqueles que são múltiplos de 3.
+'''
+
+lista = []
+lista_Multiplos = []
+for i in range (8):
+    numero = int(input("Digite um número intenrio: "))
+    lista += [numero]
+    if numero % 3 == 0:
+        lista_Multiplos += [numero]
+print(lista)
+print(f"Dentre os números digitados, os seguintes são multiplos de 3, {lista_Multiplos}.")
