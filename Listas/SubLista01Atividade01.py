f9edb331c43fc11e5c11bde58adfacaece707fbe
mateus-neres <mateusneres03@gmail.com>
2023-10-03 21:00:18 -0300

    Update SubLista01Atividade01.py

--- Listas/SubLista01Atividade01.py
+++ Listas/SubLista01Atividade01.py
@@ -10,3 +10,8 @@ d. Lista.append ( [ Lista [4], Lista [1] ] )
 e. Lista [0] += [ Lista [5] [0] ] + [ Lista [2] [2] ]
 '''
 
+Lista = [ [4, 10, 18], 14, [3, 7, 2], 16, 30, [9, 5], 12 ]
+
+Lista [0] = [Lista [6]] + Lista [0]
+
+print(Lista [5][1])
