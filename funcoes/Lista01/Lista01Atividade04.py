c6bf20664a776f40c047b556a2ad8822bd07af39
mateus-neres <mateusneres03@gmail.com>
2023-10-19 18:49:21 -0300

    Resolução de desafio
    Lista01Atividade04

--- funcoes/Lista01/Lista01Atividade04.py
+++ funcoes/Lista01/Lista01Atividade04.py
@@ -3,3 +3,17 @@
 entrada um número e exibir uma mensagem indicando se ele é primo ou não. (Dica: números primos
 só são divisíveis por 1 e por eles mesmos)
 '''
+
+import bibNumeros
+
+teste1 = [7, 4, 51, 43]
+for i in range(len(teste1)):
+    numero = bibNumeros.contaDivisores(teste1[i])
+    if numero == 2: 
+        print(teste1[i])
+        print("Primo")
+    else:
+        print(teste1[i])
+        print("Não é primo")
+    
+ 
