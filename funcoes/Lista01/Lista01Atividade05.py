bbaffcc27d3184ba6e49d177227f017984d1c816
mateus-neres <mateusneres03@gmail.com>
2023-10-19 19:08:42 -0300

    Resolução de desafio
    Lista01Atividade05

--- funcoes/Lista01/Lista01Atividade05.py
+++ funcoes/Lista01/Lista01Atividade05.py
@@ -3,3 +3,12 @@
 um caractere e retorne verdadeiro se ele for uma vogal, ou falso caso contrário. (Dica: Lembre-se que
 as vogais podem aparecer acentuadas com  ́, ^, ` ou ~.)
 '''
+
+import bibLetras
+
+teste1 = ["A", "o", "G", "5", "ã"]
+ 
+for i in range(len(teste1)):
+    vogal = str.lower(teste1[i])
+    print(vogal)
+    print(bibLetras.testaVogal(vogal))
---
+++ funcoes/Lista01/bibLetras.py
@@ -0,0 +1,6 @@
+def testaVogal (A):
+    vogal = ["a","e","i","o","u","á","é","í","ó","ú","â","ê","î","ô","û","à","è","ì","ò","ù","ã","õ"]
+    if A in vogal:
+        return True
+    else:
+        return False
