8e953212b17b533f74e54b376f3fd13a74117244
mateus-neres <mateusneres03@gmail.com>
2023-10-19 17:20:12 -0300

    Resolução de desafio
    Lista01Atividade01

--- funcoes/Lista01Atividade01.py
+++ funcoes/Lista01Atividade01.py
@@ -2,3 +2,39 @@
 As funções abaixo têm sido utilizadas por você em vários programas. Para cada uma delas, indique o
 que se pede:
 '''
+'''
+len ( ) int ( ) range ( ) input ( )
+
+Objetivo
+Quantidade de parâmetros de entrada
+Tipo(s) de parâmetro(s) de entrada
+Tipo de retorno
+
+#########   RESPOSTAS   #########
+len ( )
+Obejetivo: medir o cumprimento de uma lista
+quantidade de parametros de entrada: 01
+Tipos de parâmentros de entrada: Lista
+Tipo de retorno: int
+
+int ( )
+
+Objetivo: Converter um elemento
+Quantidade de parâmetros de entrada: 01
+Tipo(s) de parâmetro(s) de entrada: float, str
+Tipo de retorno: int
+
+range ( ) 
+
+Objetivo: gera uma sequência numérica
+Quantidade de parâmetros de entrada: 3
+Tipo(s) de parâmetro(s) de entrada: int
+Tipo de retorno: uma seguência numérica
+
+input ( )
+
+Objetivo: Receber dados do usuário
+Quantidade de parâmetros de entrada: 1
+Tipo(s) de parâmetro(s) de entrada: str
+Tipo de retorno: str
+'''
