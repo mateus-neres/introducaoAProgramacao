71b1f34f1ea0929065c8ea81d3eac94291d607be
mateus-neres <mateusneres03@gmail.com>
2023-10-19 18:00:31 -0300

    Resolução de atividade
    Lista01Atividade02

--- funcoes/lista01Atividade03.py
+++ funcoes/lista01Atividade03.py
@@ -1,5 +1,13 @@
 '''
 3) Escreva um programa que utilize as funções da biblioteca criada na questão 2 para receber como
 entrada cinco números e exibir quantos deles são múltiplos de 4.
-Entrada Saída
 '''
+import bibNumeros
+
+teste1 = [3, 9, 2, 12, 5, 0, 7, 4, 6, 8, 81, 17, 43, 11, 1 ]
+
+cont = 0
+for i in range(len(teste1)):
+    if bibNumeros.testaMultiplo4(teste1[i]) == True:
+        cont += 1
+print(cont)
