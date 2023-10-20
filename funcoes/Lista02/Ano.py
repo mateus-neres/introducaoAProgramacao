6f97ae72ef2b573d14abb117597294a47409f0a8
mateus-neres <mateusneres03@gmail.com>
2023-10-19 22:09:35 -0300

    Resolução de desafios
    Lista02Atividade03

--- funcoes/Lista02/Ano.py
+++ funcoes/Lista02/Ano.py
@@ -1,9 +1,9 @@
 def defineEstacao (A):
     if A == "JANEIRO" or A == "DEZEMBRO" or A == "FERVEREIRO":
-        return "Inverno (Vinter)"
+        return "Inverno"
     elif A == "MARÇO" or A == "ABRIL" or A == "MAIO":
-        return "Primavera (Vâr)"
+        return "Primavera"
     elif A == "JUNHO" or A == "JULHO" or A == "AGOSTO":
-        return "Verão (Sommar)"
+        return "Verão"
     else:
-        return "Outono (Hõst)"
+        return "Outono"
--- funcoes/Lista02/Lista02Atividade03.py
+++ funcoes/Lista02/Lista02Atividade03.py
@@ -3,3 +3,25 @@
 meses quaisquer (mês + quantidade de visitantes), e utilize a biblioteca Ano para calcular e
 exibir o total de visitantes registrados na primavera.
 '''
+import random
+import Ano
+# BLOCO DE TESTES
+teste1 = ["janeiro","fervereiro","março","abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro"]
+visita_teste = [88, 86, 93, 87, 83, 99, 90, 99, 94, 82, 81, 100]
+
+
+# PROGRAMA
+meses = []
+qtd_visita_primavera = 0  
+for i in range(6):
+    aleatorio = random.randint(1,11) # número aleatorio para seleção do mês
+
+    mes = str.upper(teste1[aleatorio])
+    qtd_visita = int(visita_teste[aleatorio])
+
+    if str.upper(Ano.defineEstacao(mes)) == "PRIMAVERA":
+        qtd_visita_primavera += qtd_visita
+        meses.append(mes)
+
+print(meses)
+print(qtd_visita_primavera)
