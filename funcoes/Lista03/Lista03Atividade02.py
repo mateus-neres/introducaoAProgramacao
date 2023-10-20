d025508893347573845a69f4b51cbc1115d232d7
mateus-neres <mateusneres03@gmail.com>
2023-10-20 19:25:49 -0300

    resolução de desafio

--- funcoes/Lista03/Lista03Atividade02.py
+++ funcoes/Lista03/Lista03Atividade02.py
@@ -3,29 +3,21 @@
 mensagem informando se ela dispõe de uma quantidade maior de quadros ou de esculturas.
 '''
 
-teste1 = [['Mona Lisa', 'Leonardo da Vinci', 'Pintura', 5825], ['A Noite Estrelada', 'Vincente Van Gogh', 'Pintura', 8636], ['A Última Ceia', 'Leonardo da Vinci', 'Pintura', 7752], ['O Nascimento de Vênus', 'Sandro Botticelli', 'Pintura', 8723], ['Guernica', 'Pablo Picasso', 'Pintura', 8530], ['O Beijo', 'Gustav Klimt', 'Pintura', 6915], ['O Grito', 'Edvard Munch', 'Pintura', 6923], ['A Persistência da Memória', 'Salvador Dalí', 'Pintura', 9765], ['Os Girassóis', 'Vincent van Gogh', 'Pintura', 9827], ['A Criação de Adão', 'Michelangelo', 'Pintura', 8951], ["Os Lírios D'Água", 'Claude Monet', 'Pintura', 7124], ['A Moça com o Brinco de Pérola', 'Johannes Vermeer', 'Pintura', 7210], ['A Liberdade Guiando o Povo', 'Eugène Delacroix', 'Pintura', 8947], ['Noite Estrelada sobre o Ródano', 'Vincent van Gogh', 'Pintura', 6531], ['Auto-Retrato com Macaco', 'Frida Kahlo', 'Pintura', 5038], ['O Gato e o Canário', 'Franz Marc', 'Pintura', 9983], ['Os Meninos de Varberg', 'Bruno Liljefors', 'Pintura', 5891], ['Crepúsculo', 'Caspar David Friedrich', 'Pintura', 9206], ['Primavera', 'Édouard Manet', 'Pintura', 9316], ['A Ronda Noturna', 'Rembrandt', 'Pintura', 9485], ['A Persistência da Memória', 'Salvador Dalí', 'Pintura', 9656], ['Nú de Costas', 'Amedeo Modigliani', 'Pintura', 5684], ['Noite de Verão', 'Edward Hopper', 'Pintura', 6151], ['O Filho do Homem', 'René Magritte', 'Pintura', 8843], ['O Quarto de Van Gogh em Arles', 'Vincent van Gogh', 'Pintura', 9343], ['Os Três Músicos', 'Pablo Picasso', 'Pintura', 5863], ['O Nascimento de Cristo', 'Sandro Botticelli', 'Pintura', 8938], ['Abaporu', 'Tarsila do Amaral', 'Pintura', 6997], ['Nu Azul', 'Henri Matisse', 'Pintura', 7190], ["Les Demoiselles d'Avignon", 'Pablo Picasso', 'Pintura', 6087], ['Napoleão Cruzando os Alpes', 'Jacques-Louis David', 'Pintura', 9197], ['Nenúfares', 'Claude Monet', 'Pintura', 7923], ['A Última Ceia', 'Tintoretto', 'Pintura', 8309], ['A Arte da Pintura', 'Johannes Vermeer', 'Pintura', 9118], ['Auto-Retrato com Cachimbo', 'Frida Kahlo', 'Pintura', 6022], ['O Lavrador de Café', 'Candido Portinari', 'Pintura', 6431], ['La Grande Jatte', 'Georges Seurat', 'Pintura', 5929], ['Moisés e os Dez Mandamentos', 'Rembrandt', 'Pintura', 9440], ['Os Amantes', 'René Magritte', 'Pintura', 6542], ['Cabeça de Medusa', 'Caravaggio', 'Escultura', 8301], ['Primavera', 'Sandro Botticelli', 'Pintura', 7865], ['O Guitarrista Cego', 'Pablo Picasso', 'Pintura', 9658], ['Alegoria da Primavera', 'Sandro Botticelli', 'Pintura', 5831], ['A Menina com um Brinco de Pérola', 'Johannes Vermeer', 'Pintura', 7564], ['O Grito', 'Edvard Munch', 'Pintura', 9575], ['Os Girassóis', 'Vincent van Gogh', 'Pintura', 5297], ['O Gato', 'Wassily Kandinsky', 'Escultura', 9375], ['O Banho Turco', 'Jean-Auguste-Dominique Ingres', 'Escultura', 7113], ['Retrato de Adele Bloch-Bauer I', 'Gustav Klimt', 'Pintura', 9101], ['Nenúfares e Ponte Japonesa', 'Claude Monet', 'Pintura', 9689], ['O Beijo', 'Gustav Klimt', 'Pintura', 5002], ['A Dama com Arminho', 'Leonardo da Vinci', 'Pintura', 5577], ['As Meninas', 'Diego Velázquez', 'Escultura', 5183], ['O Leão Moribundo', 'Rosa Bonheur', 'Escultura', 8153], ['Guernica', 'Pablo Picasso', 'Pintura', 5978], ['O Touro', 'Pablo Picasso', 'Escultura', 5381], ['Dama Com Unicórnio', 'Raphael', 'Pintura', 7908], ['Moça Lendo uma Carta à Luz de uma Janela Aberta', 'Johannes Vermeer', 'Pintura', 9578], ['A Última Ceia', 'Salvador Dalí', 'Pintura', 6530]]
+import bibGaleriaArte
 
-indice = len(teste1)
-
-listaObra = []
-artista = [0]
-for i in range(50):
-    obra = str.upper(teste1[i][0])
-    artista = str.upper(teste1[i][1])
-    tipo = str.upper(teste1[i][2])
-    valor = float(teste1[i][3])
-    agrupamento = (obra, artista, tipo, valor)
-    listaObra.append(agrupamento)
-
-pitura = 0
+quadro = 0
 escultura = 0
-for i in range(50):
-    if listaObra[i][2] == "PINTURA":
-        pitura += 1
-    else:
+
+for i in range (50):
+    entrada = input()
+    if(bibGaleriaArte.consultaTipo(entrada) == "Quadro"):
+        quadro += 1
+    elif(bibGaleriaArte.consultaTipo(entrada) == "Escultura"):
         escultura += 1
 
-if pitura > escultura:
-    print("Há mais pinturas que esculturas")
+if(quadro > escultura):
+    print("Mais quadros")
+elif(quadro < escultura):
+    print("Mais esculturas")
 else:
-    print("Há mais esculturas que pinturas")
+    print("Mesma quantidade")
--- funcoes/Lista03/bibGaleriaArte.py
+++ funcoes/Lista03/bibGaleriaArte.py
@@ -1,38 +1,14 @@
-'''
-Emília trabalha em uma galeria de arte e está usando um novo aplicativo para gerenciar os
-quadros e esculturas disponíveis. O aplicativo utiliza a seguinte biblioteca de funções:
+import random
 
-bibGaleriaArte
-'''
+def consultaPreco (obra):
+    return random.randint(2,8) * random.choice([132.40, 275.81, 98.66])
 
-# Função consultaPreco  recebe como entrada o título de uma obra e retorna seu valor em Reais
-def consultaPreco(A,B):
-    valor = 0
-    for i in range(len(B)):
-        if A in B[i][0]:
-            valor = B[i][3]
-    return valor
-
-# Função consultaArtista  recebe como entrada o nome de uma obra e retorna o nome do artista responsável por ela
-def consultaArtista(A, B):
-    nome = ""
-    for i in range(len(B)):
-        if A in B[i][1]:
-            nome = B[i][1]
+def consultaArtista (obra):
+    nome = random.choice(["Manoel Gomes", "Dênis Novaes", "Adélia Machado", "Patrícia Lisboa", "Leonardo Resende"])    
     return nome
 
-# Função consultaQuantObras  recebe como entrada o nome de um artista e retorna a quantidade de obras dele que existem na galeria
-def consultaQuantObras(A,B):
-    contObra = 0
-    for i in range(len(B)):
-        if A in B[i][1]:
-            contObra += 1
-    return contObra
+def consultaQuantObras (artista):
+    return random.randint(0,5)
 
-# Função consultaTipo  recebe como entrada o nome de uma obra e retorna seu tipo: Quadro ou Escultura
-def consultaTipo(A,B):
-    tipo = ""
-    for i in range(len(B)):
-        if A in B[i][0]:
-            tipo = B[i][2]
-    return tipo
+def consultaTipo (obra):
+    return random.choice(["Quadro", "Escultura"])
