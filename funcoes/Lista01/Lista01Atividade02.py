f22e79e8b1479d5b277f7e79536f008f9931013d
mateus-neres <mateusneres03@gmail.com>
2023-10-24 17:22:48 -0300

    Atualização de resolução
    Lista01Atividade 02

--- funcoes/Lista01/Lista01Atividade02.py
+++ funcoes/Lista01/Lista01Atividade02.py
@@ -1,20 +1,16 @@
-'''
-2) Crie uma biblioteca chamada bibNumeros, e crie nela:
-a) Uma função testaMultiplo4 que receba por parâmetro um número inteiro e retorne verdadeiro se
-ele for múltiplo de 4, ou falso caso contrário.
+#2) Crie uma biblioteca chamada bibNumeros, e crie nela:
 
-
-b) Uma função contaDivisores que receba como parâmetro um número inteiro, e retorne a
-quantidade de divisores que ele tem. (Dica: lembre-se de que não é possível dividir por zero)
-
-'''
 
 import bibNumeros
 
-teste1 = [4, 7, -16, 4, 5, 18, 0]
-for i in range(len(teste1)):
-    a = bibNumeros.testaMultiplo4(teste1[i])
-    b = bibNumeros.contaDivisores(teste1[i])
-    print(f"Numero testado: {teste1[i]}")
-    print(f"Divisivel por 4?: {a}")
-    print(f"Quantidade de divisões exatas: {b}")
+'''a) Uma função testaMultiplo4 que receba por parâmetro um número inteiro e retorne verdadeiro seele for múltiplo de 4,
+ou falso caso contrário.'''
+
+a = int(input("Digite um número para verificar se ele é multiplo de 4:\n"))
+print(f"{bibNumeros.testaMultiplo4(a)}")
+
+'''b) Uma função contaDivisores que receba como parâmetro um número inteiro, e retorne a
+quantidade de divisores que ele tem. (Dica: lembre-se de que não é possível dividir por zero)'''
+
+b = int(input("Digite um número para verificar a quantidade de divisores: \n"))
+print(f"Quantidade de divisões exatas: {bibNumeros.contaDivisores(b)}")
