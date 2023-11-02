3e1212da4be308f4dccbc6071b012ffc78a1b020
mateus-neres <mateusneres03@gmail.com>
2023-11-02 00:04:34 -0300

    Update Lista04Atividade07.py

--- funcoes/Lista4/Lista04Atividade07.py
+++ funcoes/Lista4/Lista04Atividade07.py
@@ -1,3 +1,8 @@
 '''7. Escreva uma função TestaOrdenacao que receba por parâmetro uma lista de números e retorne
 True caso a lista esteja em ordem crescente, ou False caso contrário. (Obs: escreva essa função
 sem usar sort)'''
+
+from bibnumeros import TestaOrdenacao
+lista = [2, 14, 39]
+print(TestaOrdenacao(lista))
+print(len(lista))
