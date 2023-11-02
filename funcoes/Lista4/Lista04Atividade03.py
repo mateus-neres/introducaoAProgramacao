b63317d7acf9803b073779a6feede8e3290762c8
mateus-neres <mateusneres03@gmail.com>
2023-11-01 23:58:18 -0300

    Update Lista04Atividade03.py

--- funcoes/Lista4/Lista04Atividade03.py
+++ funcoes/Lista4/Lista04Atividade03.py
@@ -1,2 +1,6 @@
 '''3. Escreva uma função Busca que receba por parâmetro uma lista de números e um valor, e
 retorne True caso o valor faça parte da lista, ou False caso contrário.'''
+
+from bibnumeros import Busca
+lista = [2,4,6,9]
+print(Busca(lista, 6))
