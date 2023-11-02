83bbb3b89dde5c25367d79d3163d0a80dbf8bd13
mateus-neres <mateusneres03@gmail.com>
2023-11-02 00:00:43 -0300

    Update Lista04Atividade04.py

--- funcoes/Lista4/Lista04Atividade04.py
+++ funcoes/Lista4/Lista04Atividade04.py
@@ -1,2 +1,6 @@
 '''4. Escreva uma função ContaOcorrências que receba por parâmetro uma lista de números e um
 valor e retorne a quantidade de vezes que aquele valor aparece na lista.'''
+
+from bibnumeros import ContaOcorrencias
+lista = [2,14,2,39]
+print(ContaOcorrencias(lista, 2))
