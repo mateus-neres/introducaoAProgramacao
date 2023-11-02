d8f9f52f79b770656f9ad55d60b17232aa7f2fd1
mateus-neres <mateusneres03@gmail.com>
2023-11-01 23:55:44 -0300

    Update Lista04Atividade01.py

--- funcoes/Lista4/Lista04Atividade01.py
+++ funcoes/Lista4/Lista04Atividade01.py
@@ -1,2 +1,6 @@
 '''1. Escreva uma função MediaInteiraLista que receba por parâmetro uma lista de números e
 retorne a média inteira de seus valores.'''
+
+from bibnumeros import MediaInteiraLista
+lista = []
+print(MediaInteiraLista(lista))
