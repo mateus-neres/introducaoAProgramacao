7cbe1b015066460405c68881bd96247eb858d2cf
mateus-neres <mateusneres03@gmail.com>
2023-11-02 00:03:57 -0300

    Update Lista04Atividade06.py

--- funcoes/Lista4/Lista04Atividade06.py
+++ funcoes/Lista4/Lista04Atividade06.py
@@ -1,3 +1,7 @@
 '''6. Escreva uma função SemRepeticao que receba por parâmetro uma lista de números e retorne
 uma nova versão dessa lista sem elementos de valor repetido. (Dica: use IN para saber se um
 valor está em uma lista, e + para somar listas)'''
+
+from bibnumeros import SemRepetição
+lista = [2,14,2,39]
+print(SemRepetição(lista))
