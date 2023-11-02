0308eef0f3c2c056692e5a386d1a7f07fb6f8da9
mateus-neres <mateusneres03@gmail.com>
2023-11-02 00:02:59 -0300

    Update Lista04Atividade05.py

--- funcoes/Lista4/Lista04Atividade05.py
+++ funcoes/Lista4/Lista04Atividade05.py
@@ -1,3 +1,7 @@
 '''5. Escreva uma função RemoveValor que receba por parâmetro uma lista de números e um valor e
 retorne uma nova versão dessa lista sem elementos com aquele valor. (Dica: não use remove
 nem del)'''
+
+from bibnumeros import RemoveValor
+lista = [2,14,2,39]
+print(RemoveValor(lista, 39))
