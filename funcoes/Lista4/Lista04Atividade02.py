75bcb385f5ec1fbfe614d4f0329a1a9d8303c945
mateus-neres <mateusneres03@gmail.com>
2023-11-01 23:57:23 -0300

    Update Lista04Atividade02.py

--- funcoes/Lista4/Lista04Atividade02.py
+++ funcoes/Lista4/Lista04Atividade02.py
@@ -1,3 +1,7 @@
 '''2. Escreva uma função MultiplicaLista que receba por parâmetro uma lista de números e um valor,
 e multiplique todos os elementos da lista por esse valor. A função deve retornar a nova lista
 resultante.'''
+
+from bibnumeros import MultiplicaLista
+lista = [3, 1, 0]
+print(MultiplicaLista(lista, 9))
