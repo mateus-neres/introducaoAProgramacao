c9c74e55bdf63661fa3b9cf0b8af683643d1406e
mateus-neres <mateusneres03@gmail.com>
2023-11-02 22:55:54 -0300

    Atualizações de resoluções

--- Unidade06_Funcoes/Lista03/bibGaleriaArte.py
+++ Unidade06_Funcoes/Lista03/bibGaleriaArte.py
@@ -1,9 +1,4 @@
-'''bibGaleriaArte
-
- Função consultaQuantObras  recebe como entrada o nome de um artista e
-retorna a quantidade de obras dele que existem na galeria
- Função consultaTipo  recebe como entrada o nome de uma obra e retorna
-seu tipo: Quadro ou Escultura'''
+# bibGaleriaArte
 
 #  Função consultaPreco  recebe como entrada o título de uma obra e retorna seu valor em Reais
 def consultaPreco(obra, lista):
@@ -18,3 +13,19 @@ def consultaArtista(obra, lista):
     for i in range(len(lista)):
         if obra.upper() in lista[i][1]:
             return lista[i][0]
+        
+#  Função consultaQuantObras  recebe como entrada o nome de um artista e retorna a quantidade de obras dele que existem na galeria
+def consultaQuantObras(nome, lista):
+    cont_obra = 0
+    for i in range(len(lista)):
+        if nome in lista[i][0]:
+            cont_obra += 1
+    return cont_obra
+
+#  Função consultaTipo  recebe como entrada o nome de uma obra e retorna seu tipo: Quadro ou Escultura
+def consultaTipo(obra, lista):
+    tipo_obra = ""
+    for i in range(len(lista)):
+        if obra in lista[i][1]:
+            tipo_obra = lista[i][3]
+    return tipo_obra
