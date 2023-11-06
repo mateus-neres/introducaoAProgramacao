d6a380643e0391f134a6fde8cd99090e4564ad9e
mateus-neres <mateusneres03@gmail.com>
2023-11-06 14:09:26 -0300

    Resolução de desafio
    manipulação de string:
    Lista02 Atividade 05.

--- Unidade07_Manipulacao_de_strings/Lista02/Lista02Atividade05.py
+++ Unidade07_Manipulacao_de_strings/Lista02/Lista02Atividade05.py
@@ -1,2 +1,15 @@
 # 5. Escreva uma função contaTotalH que receba como parâmetro uma lista de palavras e retorne a
 # quantidade total de letras "h", sejam maiúsculas ou minúsculas, presentes em todas elas.
+
+def contaTotalH(Lista_palavras):
+    contH = 0
+    for i in range(len(Lista_palavras)):
+        palavra = Lista_palavras[i]
+        for a in range(len(palavra)):
+            if palavra[a].upper() == "H":
+                contH += 1
+    return contH
+
+lista = ["hiato", "pato", "rinoceronte", "elefante", "hgainha", "hiena"]
+
+print(contaTotalH(lista))
