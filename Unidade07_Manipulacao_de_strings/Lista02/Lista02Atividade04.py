3e7397c925d292ceabdcfdd271936bb7222d11f7
mateus-neres <mateusneres03@gmail.com>
2023-11-06 14:00:44 -0300

    Resolução de desafio
    manipulação de string:
    Lista02 Atividade04.

--- Unidade07_Manipulacao_de_strings/Lista02/Lista02Atividade04.py
+++ Unidade07_Manipulacao_de_strings/Lista02/Lista02Atividade04.py
@@ -1,2 +1,19 @@
 # 4. Escreva uma função contaMaisDe3 que receba como parâmetro uma lista de palavras
 # e retorne a quantidade de palavras com mais de 3 vogais. (Obs: desconsidere a existência de acentos)
+
+def contaMaisDe3(lista_palavra):
+    vogal = ["a","e","i","o","u","á","é","í","ó","ú","â","ê","î","ô","û","à","è","ì","ò","ù","ã","õ"]
+    contpalavra = 0
+    for i in range(len(lista_palavra)):
+        palavra = lista_palavra[i]
+        contVogal = 0
+        for a in range(len(palavra)):
+            if palavra[a] in vogal:
+                contVogal += 1
+        if contVogal > 3:
+            contpalavra += 1
+    return contpalavra
+
+lista_palavra = ['Planaltoa', 'Agua Fria', 'Cenaatro', 'Manaira', 'Bnacaeurs', 'mtsranmaei']
+
+print(contaMaisDe3(lista_palavra))
